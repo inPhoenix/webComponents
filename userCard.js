708baaeb37c4dde1f9b7076ddaa5e3c7b8f540b7
inPhoenix <spiderk@gmail.com>
2021-03-29 14:58:22 -0300

    :sparkles: adding avatar

--- userCard.js
+++ userCard.js
@@ -6,9 +6,13 @@ template.innerHTML = `
  }
 </style>
 <div class="user-card">
+<div>
+    <img />
     <h3>
     
     </h3>
+</div>
+
 </div>
 
 `
@@ -18,6 +22,7 @@ class userCard extends HTMLElement {
         this.attachShadow({ mode: 'open' })
         this.shadowRoot.appendChild(template.content.cloneNode(true))
         this.shadowRoot.querySelector('h3').innerText = this.getAttribute('name')
+        this.shadowRoot.querySelector('img').src = this.getAttribute('avatar')
     }
 }
 window.customElements.define('user-card', userCard)
