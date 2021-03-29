f0f9dabd5781ae5d6679341d0451cb3d59185a39
inPhoenix <spiderk@gmail.com>
2021-03-29 14:52:01 -0300

    :sparkles: example with shadowRoot

--- userCard.js
+++ userCard.js
@@ -1,7 +1,23 @@
+const template = document.createElement('template')
+template.innerHTML = `
+<style>
+ h3 {
+ color: red;
+ }
+</style>
+<div class="user-card">
+    <h3>
+    
+    </h3>
+</div>
+
+`
 class userCard extends HTMLElement {
     constructor() {
         super();
-        this.innerHTML = `<h3>${this.getAttribute('name')}</h3>`
+        this.attachShadow({ mode: 'open' })
+        this.shadowRoot.appendChild(template.content.cloneNode(true))
+        this.shadowRoot.querySelector('h3').innerText = this.getAttribute('name')
     }
 }
 window.customElements.define('user-card', userCard)
